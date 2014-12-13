c7f39fe89517287ba4a9acfaf1a19b1727e0a219
Shan Ting <vxst@vxst.org>
2014-12-14 00:01:46 +0800

    Fix js ordering bug, change main repo, reduce waiting

--- tools/minify.sh
+++ tools/minify.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cat js/*.js|java -jar tools/yuicompressor-2.4.8.jar -o release/all.min.js --type js --line-break 80
+cat js/jquery.min.js js/bootstrap.js js/maker.js js/helper.js js/waiting.js js/main.js|java -jar tools/yuicompressor-2.4.8.jar -o release/all.min.js --type js --line-break 80
 cat css/*.css|java -jar tools/yuicompressor-2.4.8.jar -o release/all.min.css --type css --line-break 80
 cp index.html release/index.html
