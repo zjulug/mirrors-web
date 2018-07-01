ae6441291a51c954320e2cdbf672dc36c52941f0
Zaoqi <zaomir@outlook.com>
2018-07-01 13:05:07 +0800

    Ubuntu 18.04,18.10

--- js/main.js
+++ js/main.js
@@ -23,7 +23,9 @@ versionObj.Ubuntu={
 	"12.04":"precise",
 	"14.04":"trusty",
 	"16.04":"xenial",
-	"16.10":"yakkety"
+	"16.10":"yakkety",
+	"18.04":"bionic",
+	"18.10":"cosmic"
 };
 versionObj.Debian={
 	"6 (Squeeze)":"squeeze",
